71ab6d5335a87224b9c631f48885bc5244e81fc1
Daniel Bitsch <bitsch@leadingsystems.de>
2023-09-18 13:22:46 +0200

    optimize codestyling

--- src/Resources/contao/dca/tl_ls_data_collector.php
+++ src/Resources/contao/dca/tl_ls_data_collector.php
@@ -7,9 +7,9 @@ use Contao\DC_Table;
 
 $GLOBALS['TL_DCA']['tl_ls_data_collector'] = array
 (
-	'config' => array
+    'config' => array
     (
-		'dataContainer'               => DC_Table::class,
+        'dataContainer'               => DC_Table::class,
         'sql' => array
         (
             'keys' => array
@@ -17,73 +17,66 @@ $GLOBALS['TL_DCA']['tl_ls_data_collector'] = array
                 'id' => 'primary'
             )
         )
-	),
-
-	'list' => array
+    ),
+    'list' => array
     (
-		'sorting' => array
+        'sorting' => array
         (
-			'mode'                    => DataContainer::MODE_SORTED,
-			'flag'                    => DataContainer::SORT_INITIAL_LETTER_ASC,
-			'fields'                  => array('title'),
-			'disableGrouping'         => true,
-			'panelLayout'             => 'sort,search,limit'
-		),
-		
-		'label' => array
+            'mode'                    => DataContainer::MODE_SORTED,
+            'flag'                    => DataContainer::SORT_INITIAL_LETTER_ASC,
+            'fields'                  => array('title'),
+            'disableGrouping'         => true,
+            'panelLayout'             => 'sort,search,limit'
+        ),
+        'label' => array
         (
-			'fields' => array('title', 'alias'),
-			'format' => '<strong>%s</strong> <span style="font-style: italic;">(Alias: %s)</span>'
-		),
-		
-		'global_operations' => array
+            'fields' => array('title', 'alias'),
+            'format' => '<strong>%s</strong> <span style="font-style: italic;">(Alias: %s)</span>'
+        ),
+        'global_operations' => array
         (
-			'all' => array
-			(
-				'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
-				'href'                => 'act=select',
-				'class'               => 'header_edit_all',
-				'attributes'          => 'onclick="Backend.getScrollOffset();" accesskey="e"'
-			)
-		),
-		
-		'operations' => array
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset();" accesskey="e"'
+            )
+        ),
+        'operations' => array
         (
-			'edit' => array
+            'edit' => array
             (
-				'label'               => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['edit'],
-				'href'                => 'act=edit',
-				'icon'                => 'edit.gif'
-			),
-			'copy' => array
+                'label'               => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.gif'
+            ),
+            'copy' => array
             (
-				'label'               => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['copy'],
-				'href'                => 'act=copy',
-				'icon'                => 'copy.gif'
-			),
-			'delete' => array
+                'label'               => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
             (
-				'label'               => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['delete'],
-				'href'                => 'act=delete',
-				'icon'                => 'delete.gif',
-				'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
-			),
-			'show' => array
+                'label'               => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.gif',
+                'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
+            ),
+            'show' => array
             (
-				'label'               => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['show'],
-				'href'                => 'act=show',
-				'icon'                => 'show.gif'
-			)
-		
-		)	
-	),
-
-	'palettes' => array
+                'label'               => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+    'palettes' => array
     (
-		'default' => '{title_legend},title,alias;formId;'
-	),
-
-	'fields' => array
+        'default' => '{title_legend},title,alias;formId;'
+    ),
+    'fields' => array
     (
         'id' => array
         (
@@ -91,76 +84,76 @@ $GLOBALS['TL_DCA']['tl_ls_data_collector'] = array
         ),
         'title' => array
         (
-			'label'                   => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['title'],
-			'exclude'                 => true,
+            'label'                   => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['title'],
+            'exclude'                 => true,
             'search'                  => true,
-			'sorting'                 => true,
+            'sorting'                 => true,
             'inputType'               => 'text',
-			'flag'                    => DataContainer::SORT_ASC,
+            'flag'                    => DataContainer::SORT_ASC,
             'eval'                    => array('mandatory' => true, 'tl_class' => 'w50', 'maxlength'=>255),
             'sql'                     => "varchar(255) NOT NULL default ''"
-		),
-		'alias' => array
+        ),
+        'alias' => array
         (
-			'label'                   => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['alias'],
-			'exclude'                 => true,
+            'label'                   => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['alias'],
+            'exclude'                 => true,
             'search'                  => true,
             'sorting'                 => true,
             'flag'                    => DataContainer::SORT_ASC,
-			'inputType'               => 'text',
-			'eval'                    => array('rgxp'=>'alnum', 'doNotCopy'=>true, 'spaceToUnderscore'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
+            'inputType'               => 'text',
+            'eval'                    => array('rgxp'=>'alnum', 'doNotCopy'=>true, 'spaceToUnderscore'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
             'sql'                     => "varchar(128) BINARY NOT NULL default ''",
-			'save_callback'           => array
+            'save_callback'           => array
             (
-				array('LeadingSystems\DataCollector\ls_data_collector', 'generateAlias')
-			)
-		),
-		'formId' => array
+                array('LeadingSystems\DataCollector\ls_data_collector', 'generateAlias')
+            )
+        ),
+        'formId' => array
         (
-			'label'                   => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['formId'],
-			'exclude'                 => true,
+            'label'                   => &$GLOBALS['TL_LANG']['tl_ls_data_collector']['formId'],
+            'exclude'                 => true,
             'filter'                  => true,
-			'inputType'               => 'select',
-			'foreignKey'              => 'tl_form.title',
+            'inputType'               => 'select',
+            'foreignKey'              => 'tl_form.title',
             'sql'                     => "int(10) unsigned NOT NULL default '0'"
-		),
+        ),
         'tstamp' => array
         (
-		    'sql'                     => "int(10) unsigned NOT NULL default '0'"
-		)
-	)
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+        )
+    )
 );
 
 class ls_data_collector extends \Backend {
-	public function __construct() {
-		parent::__construct();
-	}
+    public function __construct() {
+        parent::__construct();
+    }
 
-	public function generateAlias($varValue, \DataContainer $dc) {
-		$autoAlias = false;
+    public function generateAlias($varValue, \DataContainer $dc) {
+        $autoAlias = false;
 
-		// Generate an alias if there is none
-		if ($varValue == '') {
-			$autoAlias = true;
-			$varValue = \StringUtil::generateAlias($dc->activeRecord->title);
-		}
-		$objAlias = \Database::getInstance()
-			->prepare("
-				SELECT	id
-				FROM	tl_ls_data_collector
-				WHERE	id = ?
-					OR	alias = ?
-			")
-			->execute($dc->id, $varValue);
+        // Generate an alias if there is none
+        if ($varValue == '') {
+            $autoAlias = true;
+            $varValue = \StringUtil::generateAlias($dc->activeRecord->title);
+        }
+        $objAlias = \Database::getInstance()
+            ->prepare("
+                SELECT	id
+                FROM	tl_ls_data_collector
+                WHERE	id = ?
+                    OR	alias = ?
+            ")
+            ->execute($dc->id, $varValue);
 
-		// Check whether the alias exists
-		if ($objAlias->numRows > 1) {
-			if (!$autoAlias) {
-				throw new \Exception(sprintf($GLOBALS['TL_LANG']['ERR']['aliasExists'], $varValue));
-			}
-			$varValue .= '-' . $dc->id;
-		}
+        // Check whether the alias exists
+        if ($objAlias->numRows > 1) {
+            if (!$autoAlias) {
+                throw new \Exception(sprintf($GLOBALS['TL_LANG']['ERR']['aliasExists'], $varValue));
+            }
+            $varValue .= '-' . $dc->id;
+        }
 
-		return $varValue;
-	}
+        return $varValue;
+    }
 }
